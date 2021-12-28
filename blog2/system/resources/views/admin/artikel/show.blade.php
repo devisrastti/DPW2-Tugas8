9b3d6f924b6ac82ea6ae60bc8b2bc362c0400178
devisrastti <90735838+devisrastti@users.noreply.github.com>
2021-12-28 20:00:39 +0700

    uts dpw

---
+++ blog2/system/resources/views/admin/artikel/show.blade.php
@@ -0,0 +1,20 @@
+@extends('template.admin')
+
+@section('content')
+
+<div class="card">
+    <div class="card-body">
+        <h3>Judul Artikel : {{$artikel->judul}}</h3>
+
+        <h5>Nama : {{$artikel->nama}}</h5>
+
+        <hr>
+
+        <h6>{{$artikel->isi}}</h6>
+        <p>
+            {!! nl2br($artikel->content) !!}
+        </p>
+    </div>
+</div>
+
+@endsection
